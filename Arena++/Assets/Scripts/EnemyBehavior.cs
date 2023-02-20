0847ff1f4f139ead420d27aea7c2ac3ba86ccb6e
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-02-20 14:49:04 -0700

    added script

--- Arena++/Assets/Scripts/EnemyBehavior.cs
+++ Arena++/Assets/Scripts/EnemyBehavior.cs
@@ -1,19 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform patrolRoute;
+    public List<Transform> Locations;
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+    public Transform player;
+    private int _lives = 3;
+    public int EnemyLives
+    {
+        get { return _lives; }
+        private set
+        {
+            _lives = value;
+            if (_lives <= 0)
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Enemy down.");
+            }
+        }
+
+    }
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+        InitializePatrolRoute();
+
+        MoveToNextPatrolLocation();
+    }
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in patrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return;
+        agent.destination = Locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % Locations.Count;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
     }
 
 
@@ -23,6 +66,7 @@ public class EnemyBehavior : MonoBehaviour
         //2
         if (other.name == "Player")
         {
+            agent.destination = player.position;
             Debug.Log("Player detected - Attack!");
         }
     }
@@ -35,5 +79,14 @@ public class EnemyBehavior : MonoBehaviour
             Debug.Log("Player out of range, resume patrol");
         }
     }
-    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bullet (clone)")
+        {
+            EnemyLives -= 1;
+            Debug.Log("Critical hit!");
+        }
+
+    }
+
 }
